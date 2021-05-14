bdc52fe7f763639f9a1c7e39a1698ee087348c2f
David Silva <david.silva@processmind.com.br>
2021-05-14 15:26:33 -0300

    simplifying route methods

--- src/articles/routes.rs
+++ src/articles/routes.rs
@@ -1,114 +1,88 @@
 use actix_web::{get, post, put, delete, web, HttpResponse, Responder};
 use bson::doc;
-use futures::stream::StreamExt;
-use mongodb::{options::FindOptions, Client, bson};
-use std::sync::Mutex;
-
+use mongodb::{bson};
 use bson::oid::ObjectId;
-use bson::Bson;
+use serde::{Deserialize, Serialize};
 
-const MONGO_DB: &'static str = "blog";
-const MONGO_COLLECTION_ARTICLES: &'static str = "articles";
+use crate::articles::{Article};
 
-use crate::articles::{Article, ArticleUpdateRequest, ArticleRequest};
-
-/**
- * Operações básicas do CRUD Articles
- */
-
-#[get("/articles")]
-pub async fn index(data: web::Data<Mutex<Client>>) -> impl Responder {
-    let collection = data
-        .lock()
-        .unwrap()
-        .database(MONGO_DB)
-        .collection(MONGO_COLLECTION_ARTICLES);
-
-    let filter = doc! {};
-    let find_options = FindOptions::builder().sort(doc! { "_id": -1}).build();
-
-    let mut cursor = collection.find(filter, find_options).await.unwrap();
-    let mut results = Vec::new();
-    while let Some(result) = cursor.next().await {
-        match result {
-            Ok(document) => {
-                results.push(document);
-            }
-            _ => {
-                return HttpResponse::InternalServerError().finish();
-            }
-        }
-    }
-    HttpResponse::Ok().json(results)
+// @todo -> Aprender a unificar ArticleRequest e ArticleUpdateRequest
+#[derive(Serialize, Deserialize)]
+pub struct ArticleRequest {
+    pub author: String,
+    pub title: String,
+    pub content: String,
+    pub created_at: String,
 }
 
-#[post("/articles")]
-pub async fn store(data: web::Data<Mutex<Client>>, article: web::Json<ArticleRequest>) -> impl Responder {
-    let collection = data
-        .lock()
-        .unwrap()
-        .database(MONGO_DB)
-        .collection(MONGO_COLLECTION_ARTICLES);
+#[derive(Serialize, Deserialize)]
+pub struct ArticleUpdateRequest {
+    #[serde(rename = "_id")]
+    pub _id: String,
+    pub author: String,
+    pub title: String,
+    pub content: String,
+    pub created_at: String,
+}
 
-    let doc = doc! {"author": &article.author, "created_at": &article.created_at, "title": &article.title, "content": &article.content};
+/**
+* Crud Basic Operations
+* @todo -> refactor store, remove and update methods
+*/
 
-    match collection.insert_one(doc, None).await {
-        Ok(db_result) => {
-            if let Some(new_id) = db_result.inserted_id.as_object_id() {
-                println!("New document inserted with id {}", new_id);
-            }
-            return HttpResponse::Created().json(db_result.inserted_id);
-        }
-        Err(err) => {
-            println!("Failed! {}", err);
-            return HttpResponse::InternalServerError().finish();
-        }
+#[get("/articles")]
+pub async fn index() -> impl Responder {
+    let articles = Article::_all().await;
+    
+    match articles {
+        Ok(articles) => HttpResponse::Ok().json(articles),
+        _ => HttpResponse::InternalServerError().finish()
     }
 }
 
 #[get("/articles/{id}")]
-pub async fn show(data: web::Data<Mutex<Client>>, id: web::Path<String>) -> impl Responder {
-    let collection = data
-    .lock()
-    .unwrap()
-    .database(MONGO_DB)
-    .collection(MONGO_COLLECTION_ARTICLES);
+pub async fn show(id: web::Path<String>) -> impl Responder {
+    let object_id = ObjectId::with_string(&id).unwrap();
+    let article = Article::_get_one(doc! { "_id": object_id }).await;
+    
+    match article {
+        Ok(article) => HttpResponse::Ok().json(article),
+        _ => HttpResponse::InternalServerError().finish()
+    }
+}
 
-    match collection.find_one(doc! { "_id": Bson::ObjectId(ObjectId::with_string(&id).unwrap()) }, None).await {
-        Ok(art) => HttpResponse::Ok().json(art),
-        Err(_err) => HttpResponse::InternalServerError().finish()
+#[post("/articles")]
+pub async fn store(article: web::Json<ArticleRequest>) -> impl Responder {
+    let doc = doc! {"author": &article.author, "created_at": &article.created_at, "title": &article.title, "content": &article.content};
+    let stored = Article::_create(doc).await;
+    
+    match stored {
+        Ok(stored) => HttpResponse::Ok().json(stored),
+        _ => HttpResponse::InternalServerError().finish()
     }
 }
 
 #[delete("/articles/{id}")]
-pub async fn remove(data: web::Data<Mutex<Client>>, id: web::Path<String>) -> impl Responder {
-    let collection = data
-    .lock()
-    .unwrap()
-    .database(MONGO_DB)
-    .collection(MONGO_COLLECTION_ARTICLES);
-
-    match collection.delete_one(doc! { "_id": Bson::ObjectId(ObjectId::with_string(&id).unwrap()) }, None).await {
-        Ok(art) => HttpResponse::Ok().json(art),
-        Err(_err) => HttpResponse::InternalServerError().finish()
+pub async fn remove(id: web::Path<String>) -> impl Responder {
+    let object_id = ObjectId::with_string(&id).unwrap();
+    let deleted = Article::_delete(doc! { "_id": object_id }).await;
+    
+    match deleted {
+        Ok(deleted) => HttpResponse::Ok().json(deleted),
+        _ => HttpResponse::InternalServerError().finish()
     }
 }
 
 // @todo -> Conseguir pegar o path junto com o body, só consegui pegar um dos dois
 #[put("/articles/{id}")]
-pub async fn update(data: web::Data<Mutex<Client>>, article: web::Json<ArticleUpdateRequest>) -> impl Responder {
-    let collection = data
-    .lock()
-    .unwrap()
-    .database(MONGO_DB)
-    .collection(MONGO_COLLECTION_ARTICLES);
-
-    let update = doc!{"$set": doc! {"author": &article.author, "created_at": &article.created_at, "title": &article.title, "content": &article.content}};
-    let query = doc! { "_id": Bson::ObjectId(ObjectId::with_string(&article._id).unwrap()) };
-
-    match collection.update_one(query, update, None).await {
-        Ok(art) => HttpResponse::Ok().json(art),
-        Err(_err) => HttpResponse::InternalServerError().finish()
+pub async fn update(article: web::Json<ArticleUpdateRequest>) -> impl Responder {
+    let doc = doc!{"$set": doc! {"author": &article.author, "created_at": &article.created_at, "title": &article.title, "content": &article.content}};
+    let object_id = ObjectId::with_string(&article._id).unwrap();
+    let updated = Article::_update(doc, doc! { "_id": object_id }).await;
+    
+    match updated {
+        Ok(updated) => HttpResponse::Ok().json(updated),
+        _ => HttpResponse::InternalServerError().finish()
     }
 }
 
