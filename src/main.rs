e1b5057ed3f66ff3afbba6276fc7ac9b46b40eb5
David Silva <david.silva@processmind.com.br>
2021-05-18 13:13:24 -0300

    remove old mongodb call

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use actix_web::{get, guard, web, App, HttpResponse, HttpServer, Responder};
-use mongodb::{options::ClientOptions, Client};
-
 use dotenv::dotenv;
 use std::env;
-use std::sync::*;
 
 mod articles;
 
@@ -19,7 +16,6 @@ async fn index() -> impl Responder {
 
 /**
  * Função main com init do http server
- * @todo -> Refactor main() removing old mongodb driver
  */
 
 #[actix_web::main]
@@ -28,15 +24,9 @@ async fn main() -> std::io::Result<()> {
 
     let host = env::var("HOST").expect("HOST is not set in .env file");
     let port = env::var("PORT").expect("PORT is not set in .env file");
-    
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
-    let mut client_options = ClientOptions::parse(&database_url).await.unwrap();
-    client_options.app_name = Some("blog".to_string());
-    let client = web::Data::new(Mutex::new(Client::with_options(client_options).unwrap()));
 
     HttpServer::new(move || {
         App::new()
-            .app_data(client.clone())
             .service(index)
             .configure(articles::init)
             .default_service(
